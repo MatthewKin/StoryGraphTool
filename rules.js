6598e0e7867ab641705f226d22490d7b5e1a2e07
MatthewKin <166259185+MatthewKin@users.noreply.github.com>
2025-04-18 19:44:18 -0700

    Location-specific interactive mechanism
    Fixed Box Puzzle,
    Fixed Inventory Bug

--- rules.js
+++ rules.js
@@ -1,34 +1,82 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+
+        if (!this.engine.inventory){
+            this.engine.inventory = [];
+        }
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+
+        //check if location has an item AND if item is NOT already in inventory
+        if (locationData.Item && !this.engine.inventory.includes(locationData.Item)) {
+            this.engine.inventory.push(locationData.Item);
+            //testing if stuff enters inventory properly/no dupes
+            console.log(locationData.Item + " is now in inventory"); 
+            console.log(this.engine.inventory);
+        }
+
+        //special location
+        if (this.engine.storyData.Locations[key].LocationSpecific) {
+            if (locationData.LocationSpecific === "SleepingBags") {
+                console.log("Test for SleepingBags");
+            }
+            
+            if (locationData.LocationSpecific === "PuzzleBox") {
+                console.log("PuzzleBox active");
+                this.engine.addChoice("Try to open the box", "attemptPuzzleBox");
+            }
+        }
+        
+        if(this.engine.storyData.Locations[key].Choices) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                if(choice.RequiredItem && !this.engine.inventory.includes(choice.RequiredItem)){
+                    console.log("RequiredItem Needed Here: " + choice.RequiredItem);
+                     continue; //skips entry if has Lock + doesn't have required item for lock
+                }
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
             this.engine.addChoice("The end.")
         }
+
     }
 
     handleChoice(choice) {
+        //special location input
+        if (choice === "attemptPuzzleBox") {
+            let userInput = prompt("Enter the 3-digit code:");
+            if (userInput === "549") {
+                this.engine.show("The box clicks open.");
+                this.engine.gotoScene(Location, "FlashlightFound");
+            } else {
+                this.engine.show("Nothing happens. That code didn't work.");
+                // Reload the same scene so they can try again
+                this.engine.gotoScene(Location, "Box");
+            }
+            return;
+        }
+
+
         if(choice) {
             this.engine.show("&gt; "+choice.Text);
             this.engine.gotoScene(Location, choice.Target);
         } else {
+            console.log("it went to end here");
             this.engine.gotoScene(End);
         }
     }
@@ -40,5 +88,7 @@ class End extends Scene {
         this.engine.show(this.engine.storyData.Credits);
     }
 }
+
+Engine.inventory = [];
 
 Engine.load(Start, 'myStory.json');
